36b38ecdb98e9b1fd3775866513481ad3a999e8c
Etty Soni <esoni@sfu.ca>
2020-07-27 23:03:26 -0400

    Multiple linear regression model with sckit-learn

--- linear_regression.py
+++ linear_regression.py
@@ -1,6 +1,9 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
+
+#Simple linear regression
+
 #Making the array 2d(1 column)
 #Reshape(-1, 1) makes it into a 2d array with 1 column and as many rows
 x = np.array([5, 15, 25, 35, 45, 55]).reshape((-1, 1)) 
@@ -21,4 +24,25 @@ print('intercept:', model.intercept_)
 print('slope:', model.coef_)
 
 y_pred = model.predict(x)
+print('predicted response:', y_pred, sep='\n')
+
+
+
+
+
+
+
+
+# Multiple linear regression
+x = [[0, 1], [5, 1], [15, 2], [25, 5], [35, 11], [45, 15], [55, 34], [60, 35]]
+y = [4, 5, 20, 14, 32, 22, 38, 43]
+x, y = np.array(x), np.array(y)
+
+model_1 = LinearRegression().fit(x, y)
+r_sq = model_1.score(x, y)
+print('coefficient of determination:', r_sq)
+print('intercept:', model.intercept_)
+print('slope:', model.coef_)
+
+y_pred = model_1.predict(x)
 print('predicted response:', y_pred, sep='\n')
